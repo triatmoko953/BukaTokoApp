0dcd90678076336db62770ddadefb37597329f12
anlsdwiUI <analistiana.dewi@ui.ac.id>
2023-05-07 22:51:07 +0700

    add CartController

---
+++ BukaToko/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using BukaToko.Data;
+using BukaToko.DTOS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BukaToko.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepo _cartRepo;
+
+        public CartController(ICartRepo cartRepo)
+        {
+            _cartRepo = cartRepo;
+        }
+
+        [HttpGet]
+        public IActionResult GetCart()
+        {
+            return Ok();
+        }
+    }
+}
