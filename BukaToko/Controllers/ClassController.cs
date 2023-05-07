d98e4b465b0fa1238d19086a1f6e65afc4963f1e
Tri atmoko <triatmoko95@gmail.com>
2023-05-07 17:53:03 +0700

    Add Controller

---
+++ BukaToko/Controllers/ClassController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BukaToko.Controllers
+{
+    public class ClassController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
