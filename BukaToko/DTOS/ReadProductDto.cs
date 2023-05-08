510d11a3ce085c6a074da46ff5d22fd623a936d2
Suna Alkayuni <09021181823169@student.unsri.ac.id>
2023-05-08 09:42:28 +0700

    Add ReadProductDto

---
+++ BukaToko/DTOS/ReadProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BukaToko.DTOS
+{
+    public class ReadProductDto
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
