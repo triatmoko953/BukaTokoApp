40f2d8395629c186329d2a6502430e1df17960fc
Suna Alkayuni <09021181823169@student.unsri.ac.id>
2023-05-08 09:44:34 +0700

    Add ProductProfiles

---
+++ BukaToko/Profiles/ProductProfiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BukaToko.DTOS;
+using BukaToko.Models;
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BukaToko.Profiles
+{
+    public class ProductProfiles : Profile
+    {
+        public ProductProfiles()
+        {
+            CreateMap<Product, ReadProductDto>();
+            CreateMap<CreateProductDto, Product>();
+        }
+    }
+}
