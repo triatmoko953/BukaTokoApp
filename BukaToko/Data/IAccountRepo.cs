f475f8c1dfc73729b888df9f2f4441cbe0f16422
Suna Alkayuni <09021181823169@student.unsri.ac.id>
2023-05-11 09:25:55 +0700

    Add banned to IAccount repo

--- BukaToko/Data/IAccountRepo.cs
+++ BukaToko/Data/IAccountRepo.cs
@@ -8,6 +8,6 @@ namespace BukaToko.Data
         string Register (User user);
         UserToken Login (LoginUserDto user);
         bool SaveChanges();
-        string Banned (BannedUserDto bannedUser);
+        string Banned(BannedUserDto bannedUser);
     }
 }
