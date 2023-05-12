4e4b3c1600505122297225c8a8c3b74146a10f00
anlsdwiUI <analistiana.dewi@ui.ac.id>
2023-05-13 00:52:50 +0700

    add event walletpublish

--- BukaToko/Controllers/OrderController.cs
+++ BukaToko/Controllers/OrderController.cs
@@ -150,6 +150,10 @@ namespace BukaToko.Controllers
                 var userId = await _orderRepo.GetUserId(user);
                 if (userId == null) return BadRequest("user not found");
                 await _orderRepo.Checkout(userId.Value);
+                var readWallet = await _orderRepo.GetUserId(userId.Value);
+                var walletPublishedDto = _mapper.Map<WalletPublishDto>(readWallet);
+                walletPublishedDto.Event = "Wallet_Published";
+                _messageBusClient.PublishNewWallet(walletPublishedDto);
                 return Ok();
             }
             catch (Exception ex)
--- BukaToko/Event/EventProccessor.cs
+++ BukaToko/Event/EventProccessor.cs
@@ -39,6 +39,9 @@ namespace BukaToko.Event
                 case "TopUp_Published":
                     Console.WriteLine("--> TopupWallet_NewPublished Event Detected");
                     return EventType.TopupWalletPublished;
+                case "Wallet_Published":
+                    Console.WriteLine("--> Wallet_NewPublished Event Detected");
+                    return EventType.WalletPublishedDto;
                 default:
                     Console.WriteLine("--> Could not determine the event type");
                     return EventType.Undetermined;
@@ -80,6 +83,7 @@ namespace BukaToko.Event
     enum EventType
     {
         TopupWalletPublished,
+        WalletPublishedDto,
         Undetermined
     }
 }
